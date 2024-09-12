eaee845b3ed61c1336e363fe50cc2825f829abcd
TregubovAV <5595@rambler.ru>
2024-09-12 16:26:12 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,29 @@
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+    // post.birthday = new FormDate();
+
+
+    // добавьте другие поля
+}
+
+
+//{
+//  "birthday": {
+//    "day": 13,
+//    "month": 6,
+//    "year": 1999
+//  },
+//  "name": "Иван",
+//  "passport": "4444 № 44444444",
+//  "patronymic": "Иванович",
+//  "phone": "+7 (999)-999-99-99",
+//  "surname": "Иванов",
+//  "subscription": true
+//}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    //  public FormDate birthday;
+    //  post.birthday =new
+
+    //  FormDate();
+
+    //  post.birthday.day =13;
+    //  post.birthday.month =6;
+    //  post.birthday.yaer =1999;
+
+
+}
